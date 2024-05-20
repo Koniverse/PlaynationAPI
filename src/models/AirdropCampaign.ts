1d29551c1f11341d6ce09154d2d00faae32ddf1e
Phạm Văn Khánh <khanhpv@ecotekcorp.com>
2024-05-20 18:13:41 +0700

    khanhpv - update model airdrop

--- src/models/AirdropCampaign.ts
+++ src/models/AirdropCampaign.ts
@@ -1,21 +1,29 @@
 import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
-
 import SequelizeServiceImpl from '@src/services/SequelizeService';
+
+export enum AirdropCampaignStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  CANCELED = 'CANCELED',
+}
 
 export class AirdropCampaign extends Model<InferAttributes<AirdropCampaign>, InferCreationAttributes<AirdropCampaign>> {
   declare id: CreationOptional<number>;
   declare name: string;
   declare icon: string;
-  declare start_time: Date;
-  declare end_time: Date;
+  declare start_snapshot: Date;
+  declare end_snapshot: Date;
+  declare start_claim: Date;
+  declare end_claim: Date;
+  declare eglibility_date: Date;
   declare network: string;
   declare total_tokens: number;
+  declare total_nft: number;
   declare decimal: number;
   declare method: string;
   declare raffle_count: number;
-  declare allocation: string;
-  declare steps_time: string;
   declare eligibility_criteria: JSON;
+  declare status: string;
   declare createdAt: CreationOptional<Date>;
 }
 
@@ -34,11 +42,23 @@ AirdropCampaign.init(
       type: DataTypes.STRING,
       allowNull: true,
     },
-    start_time: {
+    start_snapshot: {
       type: DataTypes.DATE,
       allowNull: false,
     },
-    end_time: {
+    end_snapshot: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    start_claim: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    end_claim: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    eglibility_date: {
       type: DataTypes.DATE,
       allowNull: false,
     },
@@ -48,7 +68,11 @@ AirdropCampaign.init(
     },
     total_tokens: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+    },
+    total_nft: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
     decimal: {
       type: DataTypes.INTEGER,
@@ -60,21 +84,21 @@ AirdropCampaign.init(
     },
     raffle_count: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    allocation: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    steps_time: {
-      type: DataTypes.JSON,
       allowNull: true,
     },
     eligibility_criteria: {
       type: DataTypes.JSON,
       allowNull: false,
     },
-
+    status: {
+      type: DataTypes.ENUM(
+        AirdropCampaignStatus.ACTIVE,
+        AirdropCampaignStatus.INACTIVE,
+        AirdropCampaignStatus.CANCELED,
+      ),
+      defaultValue: AirdropCampaignStatus.INACTIVE,
+      allowNull: false,
+    },
     createdAt: {
       type: DataTypes.DATE,
     },
