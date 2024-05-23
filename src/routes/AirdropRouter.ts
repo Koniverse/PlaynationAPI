152ec3ab8257f0fffd4aa0e9679f2ce15555d0d5
Phạm Văn Khánh <khanhpv@ecotekcorp.com>
2024-05-23 11:16:43 +0700

    khanhpv - update airdrop

--- src/routes/AirdropRouter.ts
+++ src/routes/AirdropRouter.ts
@@ -1,5 +1,5 @@
 import { IReq, IRes } from '@src/routes/types';
-import { AirdropCampaignContentCms, AirdropService } from '@src/services/AirdropService';
+import { AirdropCampaignContentCms, AirdropRecordInsertData, AirdropService } from '@src/services/AirdropService';
 import { Query } from 'express-serve-static-core';
 import { Router } from 'express';
 
@@ -7,9 +7,17 @@ const AirdropRouter = Router();
 const airdropService = AirdropService.instance;
 
 const routerMap = {
-  sync: async (req: IReq<Query>, res: IRes) => {
-    const data = req.body.data as unknown as AirdropCampaignContentCms[];
-    const response = await airdropService.syncData(data);
+  insertAirdropCampaign: async (req: IReq<AirdropRecordInsertData>, res: IRes) => {
+    const accountId = req.user?.id || 0;
+    const { typeQuery, startDate, endDate, gameId, limit } = req.body;
+    const response = await airdropService.insertAirdropCampaign(
+      accountId,
+      gameId,
+      startDate,
+      endDate,
+      limit,
+      typeQuery,
+    );
     return res.status(200).json(response);
   },
 
@@ -17,8 +25,15 @@ const routerMap = {
     const response = await airdropService.listAirdropCampaign();
     return res.status(200).json(response);
   },
+
+  sync: async (req: IReq<Query>, res: IRes) => {
+    const data = req.body.data as unknown as AirdropCampaignContentCms[];
+    const response = await airdropService.syncData(data);
+    return res.status(200).json(response);
+  },
 };
 AirdropRouter.post('/sync', routerMap.sync);
 AirdropRouter.get('/list-airdrop-campaign', routerMap.listAirdropCampaign);
+AirdropRouter.post('/insert-airdrop-record', routerMap.insertAirdropCampaign);
 
 export default AirdropRouter;
--- src/services/AirdropService.ts
+++ src/services/AirdropService.ts
@@ -1,5 +1,6 @@
 import SequelizeServiceImpl, { SequelizeService } from '@src/services/SequelizeService';
 import { AirdropCampaign, AirdropCampaignStatus } from '@src/models';
+import { QueryTypes } from 'sequelize';
 
 export interface AirdropCampaignContentCms {
   id: number;
@@ -23,6 +24,14 @@ export interface AirdropCampaignContentCms {
   status: string;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface AirdropRecordInsertData {
+  typeQuery: string;
+  startDate: string;
+  endDate: string;
+  gameId: number;
+  limit: number;
 }
 
 export class AirdropService {
@@ -51,6 +60,94 @@ export class AirdropService {
         status: AirdropCampaignStatus.ACTIVE,
       },
     });
+  }
+
+  async insertAirdropCampaign(
+    accountId: number,
+    gameId: number,
+    startDate: string,
+    endDate: string,
+    limit: number,
+    typeQuery: string,
+  ) {
+    let sql: string = '';
+    if (typeQuery === 'TOP_500_LEADERBOARD') {
+      sql = this.getAllDataQuery(gameId);
+    }
+    const data = await this.sequelizeService.sequelize.query<AirdropRecordInsertData>(sql, {
+      replacements: { accountId, gameId, startDate, endDate, limit },
+      type: QueryTypes.SELECT,
+    });
+    return data.map((item: any) => ({
+      rank: parseInt(item.rank),
+      accountId: item.accountId,
+    }));
+  }
+
+  getAllDataQuery(gameId: number) {
+    const queryGame = gameId > 0 ? 'and "gameId" = :gameId' : '';
+    return `
+        with RankedUsers as (SELECT "sourceAccountId"       AS accountId,
+                                    MIN("createdAt") as "createdAt",
+                                    SUM(coalesce(point, 0)) AS point
+                             FROM referral_log
+                             where "createdAt" >= :startDate
+                               and "createdAt" <= :endDate
+                             GROUP BY "sourceAccountId"
+                             UNION ALL
+                             SELECT "indirectAccount"                 AS accountId,
+                                    MIN("createdAt") as "createdAt",
+                                    SUM(coalesce("indirectPoint", 0)) AS point
+                             FROM referral_log
+                             where "createdAt" >= :startDate
+                               and "createdAt" <= :endDate
+                             GROUP BY "indirectAccount"
+                             UNION ALL
+                             SELECT "accountId"             AS accountId,
+                                    MIN("createdAt") as "createdAt",
+                                    SUM(coalesce(point, 0)) AS point
+                             FROM game_play
+                             where "createdAt" >= :startDate
+                               and "createdAt" <= :endDate
+            ${queryGame}
+        GROUP BY 1
+        UNION ALL
+        SELECT "accountId"             AS accountId,
+               MIN("createdAt") as "createdAt",
+               SUM(coalesce(point, 0)) AS point
+        FROM giveaway_point
+        where "createdAt" >= :startDate
+          and "createdAt" <= :endDate
+        GROUP BY 1
+        UNION ALL
+        SELECT "accountId"                     AS accountId,
+               MIN("createdAt") as "createdAt",
+               SUM(coalesce("pointReward", 0)) AS point
+        FROM task_history
+        where "createdAt" >= :startDate
+          and "createdAt" <= :endDate
+        GROUP BY 1),
+             totalData as (SELECT accountId,
+                                  sum(point) as point,
+                                  RANK()        OVER (ORDER BY sum(point) DESC, MIN("createdAt") asc) AS rank
+                           FROM RankedUsers
+                           group by 1)
+        SELECT accountId                as "accountId",
+               a."telegramUsername",
+               a.address,
+               a."firstName",
+               a."lastName",
+               a."photoUrl"             as avatar,
+               (accountId = :accountId) as mine,
+               point,
+               rank
+        FROM totalData r
+                 JOIN account a ON r.accountId = a.id
+        where rank <= :limit
+           or accountId = :accountId
+        order by rank asc;
+
+    `;
   }
 
   // Singleton this class
