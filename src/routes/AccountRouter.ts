7d90d280b8149458fcd8d50c8a4f818600893362
Khanhpv195 <khanhpoly@gmail.com>
2024-06-11 16:38:47 +0700

    KhanhPv - update account, handle banned account

--- src/routes/AccountRouter.ts
+++ src/routes/AccountRouter.ts
@@ -1,12 +1,13 @@
-import {IReq, IRes} from '@src/routes/types';
-import {Router} from 'express';
-import {Query} from 'express-serve-static-core';
-import {AccountService, GiveawayPointParams} from '@src/services/AccountService';
-import {AccountParams, GameData} from '@src/models';
+import { IReq, IRes } from '@src/routes/types';
+import { Router } from 'express';
+import { Query } from 'express-serve-static-core';
+import { AccountBanedParams, AccountService, GiveawayPointParams } from '@src/services/AccountService';
+import { AccountParams } from '@src/models';
 import jwt from 'jsonwebtoken';
 import envVars from '@src/constants/EnvVars';
-import {requireLogin, requireSecret} from '@src/routes/helper';
-import {GameService} from "@src/services/GameService";
+import { requireLogin, requireSecret } from '@src/routes/helper';
+import { GameService } from '@src/services/GameService';
+import * as console from 'node:console';
 
 type SyncAccountQuery = AccountParams & Query;
 
@@ -18,17 +19,18 @@ const routerMap = {
   // Sync account data and fetch account details
   sync: async (req: IReq<SyncAccountQuery>, res: IRes) => {
     try {
-      const {referralCode, ...data} = req.body;
+      const { referralCode, ...data } = req.body;
       const account = await AccountService.instance.syncAccountData(data, referralCode);
       const accountDetails = await accountService.fetchAccountWithDetails(account.id);
-  
-      const token = jwt.sign({
-        id: account.id,
-        address: account.address,
-        loginTime: account.sessionTime,
-      },
-      envVars.Jwt.Secret,
-      {expiresIn: '1d'},
+
+      const token = jwt.sign(
+        {
+          id: account.id,
+          address: account.address,
+          loginTime: account.sessionTime,
+        },
+        envVars.Jwt.Secret,
+        { expiresIn: '1d' },
       );
 
       return res.status(200).json({
@@ -38,7 +40,7 @@ const routerMap = {
     } catch (e) {
       console.log('Error in sync account', e);
       return res.status(400).json({
-        error: 'Invalid login data',
+        error: e.message,
       });
     }
   },
@@ -49,7 +51,7 @@ const routerMap = {
 
     const account = await accountService.findByAddress(address);
     const attributes = await accountService.getAccountAttribute(account?.id || 0);
-    
+
     const gameData = await GameService.instance.getGameDataByAccount(account?.id || 0);
 
     if (!account || !attributes) {
@@ -82,12 +84,19 @@ const routerMap = {
     return res.status(200).json(data);
   },
 
-
   syncGiveAway: async (req: IReq<GiveawayPointParams[]>, res: IRes) => {
     // @ts-ignore
     const params = req.body.data as GiveawayPointParams[];
     const data = await accountService.syncGiveAccountPoint(params);
     return res.status(200).json(data);
+  },
+
+  handleBanedAccount: async (req: IReq<Query>, res: IRes) => {
+    const data = req.body.data as unknown as AccountBanedParams[];
+    await accountService.handleBanedAccount(data);
+    return res.status(200).json({
+      success: true,
+    });
   },
 };
 
@@ -96,5 +105,6 @@ AccountRouter.get('/get-attribute', requireLogin, routerMap.getAttribute);
 AccountRouter.get('/get-rerferal-logs', requireLogin, routerMap.getReferralLog);
 AccountRouter.get('/giveaway', requireSecret, routerMap.giveAway);
 AccountRouter.post('/sync-giveaway', requireSecret, routerMap.syncGiveAway);
+AccountRouter.post('/sync-banned-account', routerMap.handleBanedAccount);
 
 export default AccountRouter;
