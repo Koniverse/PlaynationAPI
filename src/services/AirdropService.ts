1a05a41dd16bbc42dfbbd26cb4e89bfbdca90f0f
anhnhu <anhnt@subwallet.app>
2024-06-14 14:31:34 +0700

    Test claim

--- src/services/AirdropService.ts
+++ src/services/AirdropService.ts
@@ -431,7 +431,7 @@ export class AirdropService {
           address: airdropRecordLogData[0].address,
           network: airdropRecordLogData[0].network,
           decimal: airdropRecordLogData[0].decimal,
-          amount: 0.1,
+          amount: 0.2,
         };
         const sendToken: TransactionInterface = await commonService.callActionChainService(
           'chain/create-transfer',
--- src/services/LeaderBoardService.ts
+++ src/services/LeaderBoardService.ts
@@ -270,9 +270,6 @@ export class LeaderBoardService {
     limit = 100,
     typeQuery = 'all',
   ) {
-    if (!limit || limit > 100) {
-      limit = 100;
-    }
     let sql = this.getAllDataQuery(gameId);
     if (typeQuery === 'game') {
       sql = this.getGameQuery(gameId);
