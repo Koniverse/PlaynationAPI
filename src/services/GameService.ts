62c433bccf00c4f54bd0d855cd1f1db613cf2b73
anhnhu <anhnt@subwallet.app>
2024-07-24 17:07:12 +0700

    Add ratio in pointConversionRate of GamePlay

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -262,6 +262,8 @@ export class GameService {
 
     await gamePlay.update({
       point: pointRate,
+      gamePoint: point,
+      ratio: game.pointConversionRate,
       endTime: new Date(),
       success: true,
     });
